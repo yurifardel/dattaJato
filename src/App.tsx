1f8f150cb69e3789d89ccd76b9c52a17e815ba12
yurifardel <yrnascimento@outlook.com>
2021-07-06 01:49:05 -0300

    first update

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
+import { Fragment } from 'react';
+import React from 'react';
+
+import car from './assets/car.jpeg';
+
+
 import './App.css';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>app</h1>
-    </div>
+    <Fragment>
+      <div className="Container">
+        <div className="Content">
+          <form action="">
+            <input type="text" />
+            <input type="text" />
+            <a href="/">
+              <h1>Entrar</h1>
+            </a>
+          </form>
+
+         <a href="/">
+           <p>Registre-se</p>
+         </a>
+        </div>
+
+        <div className="ImgCar">
+          <img src={car} alt="car" />
+        </div>
+      </div>
+    </Fragment>
   );
 }
 
