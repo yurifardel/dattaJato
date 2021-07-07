bdf423a5e60458ea9123c765ee132422d5cb1445
yurifardel <yrnascimento@outlook.com>
2021-07-06 21:47:06 -0300

    up

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import { Fragment } from 'react';
-import React from 'react';
-
-import car from './assets/car.jpeg';
-
-
-import './App.css';
-
+import Home from './components/Home/Home';
+import './Global.css'
 
 function App() {
   return (
     <Fragment>
-      <div className="Container">
-        <div className="Content">
-          <form action="">
-            <input type="text" />
-            <input type="text" />
-            <a href="/">
-              <h1>Entrar</h1>
-            </a>
-          </form>
-
-         <a href="/">
-           <p>Registre-se</p>
-         </a>
-        </div>
-
-        <div className="ImgCar">
-          <img src={car} alt="car" />
-        </div>
-      </div>
+      <Home/>
     </Fragment>
   );
 }
