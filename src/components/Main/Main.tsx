adc86a3f723464bbe55bdda1fddd27a8edd26ef0
yurifardel <yrnascimento@outlook.com>
2021-07-08 22:31:23 -0300

    created component main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,18 @@
+import './style.css'
+
+function Main(){
+ return(
+   <div className="container">
+     <main className="ContainerMain">
+      <button className="ButtonMain" type="button">
+          <h3>Faça seu login</h3>
+      </button>
+   </main>
+   </div>
+   
+ )
+}
+
+export default Main;
+
+
