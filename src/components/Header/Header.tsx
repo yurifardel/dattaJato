821c9c5a39f66191e08327a474610747e74bdf4b
yurifardel <yrnascimento@outlook.com>
2021-07-08 22:31:04 -0300

    created component header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import './style.css'
+
+function Header(){  
+ return(
+    <header className="Container">
+      <a className="LinkHeaderLogo" href="/">
+        <img className="Image" src="https://fontmeme.com/permalink/210709/83fb72aa95acd17e2294271d23bdfcdb.png" alt="netflix-font"  style={{border: "none"}} />  
+      </a>  
+    </header>
+ )
+}
+
+export default Header;
+
