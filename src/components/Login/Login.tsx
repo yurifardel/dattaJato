cea408bcfcb050ade73e8aa28a015720f5ab2288
yurifardel <yrnascimento@outlook.com>
2021-07-12 00:02:21 -0300

    created login component

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,7 +2,14 @@ function Login(){
   return(
     <div className="ContainerLogin">
       <div className="ContentForm">
-
+        <h1>Faça seu login</h1>
+        <form action="">
+          <input type="text"  placeholder="Digite seu email"/>
+          <input type="password" placeholder="Digite sua senha"/>
+          <button>
+            <p>Entrar</p>
+          </button>
+        </form>
       </div>
     </div>
   )
