70b0a05f1e8f77f52565a0e8bde8166af2ee531a
yurifardel <yrnascimento@outlook.com>
2021-07-13 00:52:18 -0300

    att

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,3 +1,7 @@
+import {Link} from 'react-router-dom';
+
+import './style.css'
+
 function Login(){
   return(
     <div className="ContainerLogin">
@@ -6,9 +10,16 @@ function Login(){
         <form action="">
           <input type="text"  placeholder="Digite seu email"/>
           <input type="password" placeholder="Digite sua senha"/>
-          <button>
-            <p>Entrar</p>
-          </button>
+          <Link to="/auth/sucess" id="ButtonId">
+            <button>
+              <p>Entrar</p>
+            </button>
+          </Link>
+          
+
+          <Link to="/auth/register" id="RegisterLink">
+            Clique para cadastrar
+          </Link>
         </form>
       </div>
     </div>
