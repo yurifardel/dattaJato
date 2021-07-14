028d83c8895355e0824ded083445ef94b3958e9a
yurifardel <yrnascimento@outlook.com>
2021-07-14 00:42:33 -0300

    att

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -4,25 +4,27 @@ import gifCar from '../../assets/carro.gif';
 
 function Login(){
   return(
-    <div className="ContainerLogin">
-     
-      <div className="ContentForm">
-        <h1>Faça seu login</h1>
-        <form action="">
-          <input type="e-mail"  placeholder="Digite seu email"/>
-          <input type="password" placeholder="Digite sua senha"/>
-          <Link to="/auth/sucess" id="ButtonId">
-            <button>
-              <p>Entrar</p>
-            </button>
-          </Link>
+    <div className="container">
+      <div className="ContainerLogin">
+        <img src={gifCar} alt="" />
+        <div className="ContentForm">
+          <h1>Faça seu login</h1>
+          <form action="">
+            <input type="e-mail"  placeholder="Digite seu email"/>
+            <input type="password" placeholder="Digite sua senha"/>
+            <Link to="/auth/sucess" id="ButtonId">
+              <button>
+                <p>Entrar</p>
+              </button>
+            </Link>
 
-          <Link to="/auth/register" id="RegisterLink">
-            <p >Clique para cadastrar</p> 
-          </Link>
-        </form>
+            <Link to="/auth/register" id="RegisterLink">
+              <p >Clique para cadastrar</p> 
+            </Link>
+          </form>
+        </div>
+        
       </div>
-      <img src={gifCar} alt="" />
     </div>
   )
 }
