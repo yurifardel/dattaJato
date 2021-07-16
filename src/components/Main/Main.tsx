dd1a974950f71675163603b2ee55ad08656f9444
yurifardel <yrnascimento@outlook.com>
2021-07-16 12:13:39 -0300

    bugs fixes in the main

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,17 +3,13 @@ import {Link} from 'react-router-dom'
 
 function Main(){
  return(
-   <div className="container">
      <main className="ContainerMain">
        <Link to='/auth/login'>
         <button className="ButtonMain" type="button">
           <h3>Faça seu login</h3>
         </button>
        </Link>
-      
-   </main>
-   </div>
-   
+   </main>   
  )
 }
 
