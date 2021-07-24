b15fa1311efded10b9c59225b910e1714c707a00
yurifardel <yrnascimento@outlook.com>
2021-07-24 20:26:26 -0300

    ...

--- src/components/Register/Register.tsx
+++ src/components/Register/Register.tsx
@@ -1,6 +1,45 @@
-function Register(){
-  return(
-    <div></div>
+import {Link} from 'react-router-dom';
+import './style.css';
+
+function Register() {
+  return (
+    <div className="Container">
+      <div className="content">
+        <form action="" method="get">
+          <fieldset>
+            <legend>Registre-se</legend>
+
+            <div className="field-group">
+              <div className="field">
+                <label htmlFor="name">Digite seu nome</label>
+                <input type="text" name="name" required />
+              </div>
+              <div className="field">
+                <label htmlFor="name">Digite seu email</label>
+                <input type="e-mail" name="email" required />
+              </div>
+              <div className="field">
+                <label htmlFor="name">Digite seu CPF</label>
+                <input type="text" name="cpf" required />
+              </div>
+              <div className="field">
+                <label htmlFor="name">Digite sua senha</label>
+                <input type="password" name="senha" required />
+              </div>
+              <div className="field">
+                <label htmlFor="name">Confirme sua senha</label>
+                <input type="password" name="senha" required />
+              </div>
+            </div>
+          </fieldset>
+        </form>
+        <button className="button">
+          <Link to="/">
+            <p>Enter</p>
+          </Link>
+        </button>
+      </div>
+    </div>
   );
 }
 
