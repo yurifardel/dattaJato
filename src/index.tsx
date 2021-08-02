3bab734c8c3de7f6f723e0a177eea65a78e684df
yurifardel <yrnascimento@outlook.com>
2021-08-02 00:46:32 -0300

    created and update files

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import Home from './components/Home/Home';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Layout from './components/Layout/Layout';
+import Cards from './components/Cards/Cards';
+
 
 import {BrowserRouter, Switch, Route} from "react-router-dom";
 
@@ -11,8 +14,10 @@ ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" exact component={Home}/>
-      <Route path="/auth/login" component={Login}/>
-      <Route path="/auth/register" exact component={Register}/>
+      <Route path="/login" component={Login}/>
+      <Route path="/register" exact component={Register}/>
+      <Route path="/auth/login" component={Layout}/>
+      <Route path="/auth/card" component={Cards}/>
 
     </Switch>
   </BrowserRouter>,
